75b263cfa481175953200432f4f5822b200c928c
munakbey <munakbey@gmail.com>
2020-04-09 23:59:16 +0300

    new init updated

--- controller/kullanici-istek-controller.js
+++ controller/kullanici-istek-controller.js
@@ -12,4 +12,29 @@ router.get('/list', (req, res) => {
 );
 });
 
+router.put("/:id", async (request, response) => {
+    try {
+        var person = await KullaniciIstek.findById(request.params.id).exec();
+        person.set(request.body);
+        var result = await person.save();
+        response.send(result);
+    } catch (error) {
+        response.status(500).send(error);
+    }
+});
+
+router.post("/", function(req, res, next){
+ 
+    KullaniciIstek({
+        plaka: req.body.plaka,
+        camId_1: req.body.camId_1,
+        camId_2: req.body.camId_2,
+        mesafe: req.body.mesafe,
+    }).save().then(() => {
+        res.json("Kaydetme İşlemi Başarılı."+req.body.plate);
+    }).catch((err) => {
+        res.json("Kaydetme İşleminde Hata Oluştu.");
+    });
+});
+
 module.exports = router;
