8edc4201ecefdf13b2e7a536aab68ec202918400
munakbey <munakbey@gmail.com>
2020-05-06 01:24:53 +0300

    nolur bit artık

--- controller/kullanici-istek-controller.js
+++ controller/kullanici-istek-controller.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 var KullaniciIstek=mongoose.model("KullaniciIstek")
 
 var router = express.Router();
+var count;
 
 router.get('/list', (req, res) => {
     KullaniciIstek.find((err, data) => {
@@ -24,22 +25,54 @@ router.put("/:id", async (request, response) => {
 });
 router.post("/", function(req, res, next){
 
-    KullaniciIstek({
-        plaka: req.body.plaka,
-        hız: req.body.hız,
-        mesafe: req.body.mesafe,
-        lat1: req.body.lat1,
-        long1: req.body.long1,
-        lat2: req.body.lat2,
-        long2: req.body.long2,
-        adres: req.body.adres,
-        adres2: req.body.adres2,
-        tarih: req.body.tarih,
-    }).save().then(() => {
-        res.json("Kaydetme İşlemi Başarılı."+req.body.plate);
-    }).catch((err) => {
-        res.json("Kaydetme İşleminde Hata Oluştu.");
-    });
+    KullaniciIstek
+    .find({plate: req.params.plate}, { 
+        
+    }, (err, doc) => {
+        console.log(doc+"$$$$$");
+      console.log(doc+"---"+res);
+
+
+      KullaniciIstek
+      .find({adres:doc.adres,adres2:doc.adres2,tarih:doc.tarih,mesafe:doc.mesafe}, { 
+      }, (err, doc) => {
+          res.json(doc);
+          console.log(doc+"_________##");
+        count=doc;
+      }).count();
+  
+    if(count==0){
+        
+        KullaniciIstek({
+            plaka: req.body.plaka,
+            hız: req.body.hız,
+            mesafe: req.body.mesafe,
+            lat1: req.body.lat1,
+            long1: req.body.long1,
+            lat2: req.body.lat2,
+            long2: req.body.long2,
+            adres: req.body.adres,
+            adres2: req.body.adres2,
+            tarih: req.body.tarih,
+        }).save().then(() => {
+            res.json("Kaydetme İşlemi Başarılı."+req.body.plate);
+        }).catch((err) => {
+            res.json("Kaydetme İşleminde Hata Oluştu.");
+        });
+
+
+    }
+    
+    
+    
+    
+    
+    }).sort({time: -1});
+
+
+
+
+ 
 });
 
 
@@ -51,4 +84,7 @@ router.delete('/sil/:id', (req, res) => {
         else { console.log('Silme hatası :' + err); }
     });
 });
+
+
+
 module.exports = router;
